2fe2bc39a5cd6717f64e40b849cffffd8ea328e2
marcio.santos <marcio.santos@redfox.tech>
2021-07-01 20:26:22 -0300

    send project to heroku

--- src/main/java/com/marcio/springbootapi/services/DbTestService.java
+++ src/main/java/com/marcio/springbootapi/services/DbTestService.java
@@ -60,6 +60,7 @@ public class DbTestService {
 		Category cat5 = new Category(null, "Gardening");
 		Category cat6 = new Category(null, "Decoration");
 		Category cat7 = new Category(null, "Perfume");
+		Category cat8 = new Category(null, "teste");
 
 		Product p1 = new Product(null, "NoteBook", 2000.0);
 		Product p2 = new Product(null, "Printer", 800.0);
@@ -93,7 +94,7 @@ public class DbTestService {
 		p10.getCategories().addAll(Arrays.asList(cat6));
 		p11.getCategories().addAll(Arrays.asList(cat7));
 
-		categoryRepo.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+		categoryRepo.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7,cat8));
 		productRepo.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
 
 		State stt1 = new State(null, "Minas Gerais");
