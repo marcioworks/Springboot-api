a0089961419c8ad55e4bdc31ec5c5078d20608cf
marcio silva <marcioworkar@gmail.com>
2021-07-07 22:16:57 -0300

    s3 service

--- src/main/java/com/marcio/springbootapi/SpringbootApiApplication.java
+++ src/main/java/com/marcio/springbootapi/SpringbootApiApplication.java
@@ -1,13 +1,17 @@
 package com.marcio.springbootapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.marcio.springbootapi.services.S3Service;
+
 @SpringBootApplication
 public class SpringbootApiApplication implements CommandLineRunner {
 
-	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApiApplication.class, args);
@@ -16,7 +20,7 @@ public class SpringbootApiApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-	
+		s3Service.uploadFile("/home/marcioss/Pictures/foto.jpeg");
 
 	}
 
